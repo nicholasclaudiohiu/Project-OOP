7605e9632ee50b4a425b043431b210544b5f1c98
Nicholas Claudio <nicholasclaudiohiu@gmail.com>
2025-09-03 12:45:25 +0700

    second commit

--- src/Week2_Nicholas/id/ac/umn/Tutorial1.java
+++ src/Week2_Nicholas/id/ac/umn/Tutorial1.java
@@ -1,10 +1,30 @@
 package Week2_Nicholas.id.ac.umn;
 
+import java.util.Scanner;
+
 public class Tutorial1 {
-
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		String[] matkulIF = {"Matematika Discrit", "Dasar-Dasar Pemrograman", "Pemrograman Berorientasi objek"};
+		String[] matkulCE = {"Riset Operasi", "Jaringan Komputer", "Aljabar Linear"};
+		String[] matkulIS = {"Sistem Database", "Administrasi Database"};
+		System.out.println("Pilih Kategori Mata Kuliah : ");
+		System.out.println("1. Informatika\n2. Computer Engineering\n3. Informative System");
+		System.out.println("Pilih : ");
+		int input = scanner.nextInt();
+		
+		switch(input) {
+		case 1: System.out.println("Daftar Mata Kuliah : "); show(matkulIF); break;
+		case 2: System.out.println("Daftar Mata Kuliah : "); show(matkulCE); break;
+		case 3: System.out.println("Daftar Mata Kuliah : "); show(matkulIS); break;
+		default:  System.out.println("Pilihan Tidak Valid");
+		}
 	}
-
+	public static void show(String[] matkul) {
+		for(int i = 0; i < matkul.length; i++) {
+			System.out.println("- " + matkul[i]);
+		}
+	}
 }
